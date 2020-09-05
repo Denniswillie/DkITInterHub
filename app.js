f28fd1f75fa0105b09aba0e53720540f65c3e4d2
unknown <denniswillie2000@gmail.com>
2020-09-05 18:11:53 +0100

    install passport, passport-local, passport-local-mongoose, express-session packages

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+const passport = require("passport");
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/ejsDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/dkitInterHubDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.get("/", function(req, res) {
 
