cd3ccb5209c1ecea8afeed883e3d188a89ead06e
unknown <denniswillie2000@gmail.com>
2020-09-05 23:04:58 +0100

    add google authentication but still not structured

--- app.js
+++ app.js
@@ -9,19 +9,124 @@ const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const ObjectId = require("mongodb").ObjectID;
+const findOrCreate = require("mongoose-findorcreate");
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+  secret: "d414d0_f1r1c15",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/dkitInterHubDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
 
-app.get("/", function(req, res) {
-
+const ratingSchema = new mongoose.Schema({
+  rate: Number,
+  description: String
 });
 
-app.post("/", function(req, res) {
+const itemSchema = new mongoose.Schema({
+  merchantStoreId: ObjectId,
+  name: String,
+  price: Number,
+  image: String,
+  description: String
+});
 
+const storeSchema = new mongoose.Schema({
+  creatorId: ObjectId,
+  items: [itemSchema]
+});
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  googleId: String,
+  name: String,
+  country: String,
+  rating: ratingSchema,
+  store: storeSchema,
+  phoneNumber: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/dashboard",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get("/", function(req, res) {
+  res.render("register");
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ["profile"] })
+);
+
+app.get("/dashboard", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("dashboard");
+  } else {
+    res.redirect("/register");
+  }
+});
+
+app.get('/auth/google/dashboard',
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect dashboard.
+    res.redirect('/dashboard');
+});
+
+app.post("/register", function(req, res) {
+  User.register({username: req.body.username}, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/dashboard");
+      })
+    }
+  })
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
