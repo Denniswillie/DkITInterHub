6ed6b16dcf27ada5e085e83875e9be931de0fea5
unknown <denniswillie2000@gmail.com>
2020-09-16 16:25:41 +0100

    add working autocomplete suggestions for existing usernames on createRoom page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router  = express.Router();
+const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 const schemas = require("../schemas");
@@ -9,14 +9,21 @@ const userSchema = schemas.userSchema;
 const roomSchema = schemas.roomSchema;
 const Room = new mongoose.model("Room", roomSchema);
 const authRoutes = require("../routes/auth");
-const {Storage} = require('@google-cloud/storage');
-const multer  = require('multer')
-const upload = multer({ dest: '../userProfileImage' });
+const {
+  Storage
+} = require('@google-cloud/storage');
+const multer = require('multer')
+const upload = multer({
+  dest: '../userProfileImage'
+});
 
 // Authenticate google cloud storage client and create bucket.
 const projectId = 'dkitinterhub'
 const keyFilename = './DkitInterHub-18ea7da7837a.json'
-const storage = new Storage({projectId, keyFilename});
+const storage = new Storage({
+  projectId,
+  keyFilename
+});
 const bucket = storage.bucket('first_test_bucket_dkitinterhub');
 
 // Setup server requests and responses on different routes.
@@ -37,8 +44,11 @@ router.get("/dashboard", function(req, res) {
         const fileName = req.user._id + ".img";
         const file = bucket.file(fileName);
         createOrUpdateUserProfileImage(req, file)
-           .then(res.render("dashboard", {user: req.user, contents:allContents}))
-           .catch((err) => console.log(err));
+          .then(res.render("dashboard", {
+            user: req.user,
+            contents: allContents
+          }))
+          .catch((err) => console.log(err));
       });
     }
   } else {
@@ -46,24 +56,29 @@ router.get("/dashboard", function(req, res) {
   }
 });
 
-router.get("/logout", function(req, res){
+router.get("/logout", function(req, res) {
   req.logout();
   res.redirect("/");
 });
 
-router.get("/createContent", function(req, res){
+router.get("/createContent", function(req, res) {
   res.render("createContent");
 })
-router.post("/createContent", function(req, res){
+router.post("/createContent", function(req, res) {
   var title = req.body.title;
   var type = req.body.type;
   var content = req.body.content;
   var invitation_url = req.body.invitation_url;
 
-  ContentCard.create({title:title, type:type, content:content, invitation_url:invitation_url}, function(err, theContent){
-    if (err){
+  ContentCard.create({
+    title: title,
+    type: type,
+    content: content,
+    invitation_url: invitation_url
+  }, function(err, theContent) {
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       console.log(theContent);
       res.redirect("/dashboard");
     }
@@ -80,8 +95,8 @@ router.post("/userProfileImage", upload.single('userProfileImage'), function(req
 
   bucket.upload(req.file.path, options, function(err, file) {
     createOrUpdateUserProfileImage(req, file)
-        .then(res.redirect("/"))
-        .catch((err) => console.log(err));
+      .then(res.redirect("/"))
+      .catch((err) => console.log(err));
   });
 });
 
@@ -96,7 +111,11 @@ async function createOrUpdateUserProfileImage(req, file) {
       console.error(err);
       return;
     }
-    User.findOneAndUpdate({_id: req.user._id}, {imageUrl: url}, function(err, foundUser) {
+    User.findOneAndUpdate({
+      _id: req.user._id
+    }, {
+      imageUrl: url
+    }, function(err, foundUser) {
       if (err) {
         console.log(err);
         return;
@@ -112,15 +131,18 @@ router.get("/userProfileInput", function(req, res) {
 router.post("/usernameAvailabilityChecker", function(req, res) {
   const username = req.body.username;
   const User = new mongoose.model("User", userSchema);
-  User.find({username: { $regex: username, $options: "i" }}, function(err, foundUser) {
+  User.find({
+    username: {
+      $regex: username,
+      $options: "i"
+    }
+  }, function(err, foundUser) {
     if (err) {
       console.log(err);
       return;
-    }
-    else if (foundUser.length == 0) {
+    } else if (foundUser.length == 0) {
       res.send("Username is available!");
-    }
-    else {
+    } else {
       res.send("Username not available!");
     }
   });
@@ -131,7 +153,13 @@ router.post("/userProfileInput", function(req, res) {
   const country = req.body.country;
   const course = req.body.course;
   const User = new mongoose.model("User", userSchema);
-  User.findOneAndUpdate({_id: req.user._id}, {username: username, country: country, course: course}, function(err, foundUser) {
+  User.findOneAndUpdate({
+    _id: req.user._id
+  }, {
+    username: username,
+    country: country,
+    course: course
+  }, function(err, foundUser) {
     if (err) {
       console.log(err);
       return;
@@ -152,20 +180,71 @@ router.get("/createRoom", function(req, res) {
 router.post("/existingUsers", function(req, res) {
   const User = new mongoose.model("User", userSchema);
   const startingLettersRegex = "^" + req.body.inputElement;
-  User.find({
-    $and: [
-      {username: {$regex: startingLettersRegex, $options: "i"}},
-      {username: {$not: {$eq: req.user.username}}}
-    ]
-  }, function(err, foundUsers) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(foundUsers);
-    }
-  })
-  .limit(5);
+  findUserThatHasMatchingUsername(req)
+    .then((matchedUser) => {
+      User.find({
+          $and: [{
+            username: {
+              $regex: startingLettersRegex,
+              $options: "i"
+            }
+          }, {
+            username: {
+              $not: {
+                $eq: req.user.username
+              }
+            }
+          }, {
+            username: {
+              $not: {
+                $eq: matchedUser.username
+              }
+            }
+          }]
+        }, function(err, foundUsers) {
+          if (err) {
+            console.log(err);
+          } else if (matchedUser.foundUser != undefined) {
+            foundUsers.unshift(matchedUser.foundUser);
+            res.send(foundUsers);
+          } else {
+            res.send(foundUsers);
+          }
+        })
+        .limit(4);
+    });
 });
+
+async function findUserThatHasMatchingUsername(req) {
+  return new Promise(function(resolve, reject) {
+      const User = new mongoose.model("User", userSchema);
+      User.findOne({
+        $and: [{
+          username: req.body.inputElement
+        },
+        {username: {
+          $not: {
+            $eq: req.user.username
+          }
+        }}]
+      }, function(err, foundUser) {
+        if (err) {
+          console.log(err);
+        } else if (foundUser) {
+          resolve({
+            username: foundUser.username,
+            foundUser: foundUser
+          });
+        }
+        resolve({
+          username: ""
+        });
+      });
+    })
+    .then((foundUser) => {
+      return foundUser;
+    });
+}
 
 router.post("/createRoom", function(req, res) {
 
